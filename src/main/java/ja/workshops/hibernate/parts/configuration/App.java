6a51d3076ec80345c79ddb2b5e4316a97741840d
Krzysiek <krzysztof.niedzielski96@gmail.com>
2019-06-12 14:27:33 +0200

    config task in new way

--- src/main/java/ja/workshops/hibernate/parts/configuration/App.java
+++ src/main/java/ja/workshops/hibernate/parts/configuration/App.java
@@ -1,17 +1,17 @@
-package ja.workshop.hibernate;
+package ja.workshops.hibernate.parts.configuration;
 
-import ja.workshop.hibernate.connectors.H2Connector;
-import ja.workshop.hibernate.connectors.MysqlConnector;
-import ja.workshop.hibernate.connectors.PostgresConnector;
+import ja.workshops.hibernate.parts.connectors.H2Connector;
+import ja.workshops.hibernate.parts.connectors.MysqlConnector;
+import ja.workshops.hibernate.parts.connectors.PostgresConnector;
 
 /**
  * @author krzysztof.niedzielski
  */
 public class App {
+
     public static void main(String[] args) {
         new H2Connector().getSession();
         new MysqlConnector().getSession();
         new PostgresConnector().getSession();
     }
-
 }
